75e44ec54651e16a84506970747bc1f90cb2410f
Franz Cordes-Eklund <franzcordeseklund@gmail.com>
2021-02-01 18:03:00 -0600

    test

--- src/assets/mail/contact_me.js
+++ src/assets/mail/contact_me.js
@@ -21,7 +21,7 @@ $(function () {
             $this = $("#sendMessageButton");
             $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
             $.ajax({
-                url: "/src/assets/mail/contact_me.php",
+                url: "https://formspree.io/f/xoqpbjow",
                 type: "POST",
                 data: {
                     name: name,
